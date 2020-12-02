ff0380f105211c4945886ad6494db170683daa7b
Aras <amvitkus@gmail.com>
2020-12-01 20:17:11 -0500

    Initial commit

---
+++ android/src/main/java/com/arasvitkus/voiddash/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.arasvitkus.voiddash.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.arasvitkus.voiddash.VoidDash;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new VoidDash(), configuration);
+	}
+}
